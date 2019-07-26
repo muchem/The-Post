7c4d4b3e7b80e0cf6c5f3a26949b6e9ef9d174b9
muchemm <melissauchem@gmail.com>
2019-07-26 13:25:42 -0700

    rewrite js

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-window.onload = function(){
-   
-    async function getPost(){
-        const htmlLeft = document.getElementById('left-section');
-        let outputLeft = "";
-        const htmlRight = document.getElementById('Right-section');
-        let outputRight = "";
-        let linktx = "Continue reading here";
-        let imgalt = "image not avalablie"
-       
-        const api = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${'229d43347db64925ac4249aacfd8559b'}`;
-        const responce = await fetch(api);
-        const data =  await responce.json();
-        console.log(data);
-        
 
-     for(let i=0; i <= 9; i++){
-      outputLeft +=  "<h1>" + data.articles[i].title +
-      "<h6>" + data.articles[i].publishedAt ;
-      outputLeft += "<img src = '" + data.articles[i].urlToImage + "' " + "alt='" + imgalt + "'>";
-      outputLeft += "<br>" +  "<h5>" + data.articles[i].description + "<br>" +
-      "<p>" + data.articles[i].content + "<br>";
-      outputLeft += "<br>"+ "<a href = '" + data.articles[i].url +"' " + "target='" + "_blank"+ "'>"+ linktx +'</a>'; 
-     }
+    async function getPost(){
+      const htmlLeft = document.getElementById('left-section');
+      let outputLeft = "";
+      const htmlRight = document.getElementById('Right-section');
+      let outputRight = "";
+      let linktx = "Continue reading here";
+      let imgalt = "image not available";
+       
+      const api = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${'229d43347db64925ac4249aacfd8559b'}`;
+      const responce = await fetch(api);
+      const data =  await responce.json();
+
+   for(let i=0; i <= 9; i++){
+   outputLeft +=  "<h1>" + data.articles[i].title +
+   "<h6>" + data.articles[i].publishedAt ;
+   outputLeft += "<img src = '" + data.articles[i].urlToImage + "' " + "alt='" + imgalt + "'>";
+   outputLeft += "<br>" +  "<h5>" + data.articles[i].description + "<br>" +
+   "<p>" + data.articles[i].content + "<br>";
+   outputLeft += "<br>"+ "<a href = '" + data.articles[i].url +"' " + "target='" + "_blank"+ "'>"+ linktx +'</a>'; 
+
+   }
      htmlLeft.innerHTML = outputLeft;
     
-     for(let i = 10; i<=19; i++){
-        outputRight +=  "<h2>" + data.articles[i].title +
-        "<h6>" + data.articles[i].publishedAt ;
-        outputRight += "<img src = '" + data.articles[i].urlToImage + "' " + "alt='" + imgalt + "'>";
-        outputRight +=
-        "<h5>" + data.articles[i].description + "<br>" +
-        "<p>" + data.articles[i].content + "<br>" ;
-        outputRight += "<br>"+ "<a href = '" + data.articles[i].url +"' " + "target='" + "_blank"+ "'>"+ linktx +'</a>'; 
-     }
+   for(let i = 10; i<=19; i++){
+   outputRight +=  "<h2>" + data.articles[i].title +
+   "<h6>" + data.articles[i].publishedAt ;
+   outputRight += "<img src = '" + data.articles[i].urlToImage + "' " + "alt='" + imgalt + "'>";
+   outputRight +=
+   "<h5>" + data.articles[i].description + "<br>" +
+   "<p>" + data.articles[i].content + "<br>" ;
+   outputRight += "<br>"+ "<a href = '" + data.articles[i].url +"' " + "target='" + "_blank"+ "'>"+ linktx +'</a>'; 
+
+   }
      htmlRight.innerHTML = outputRight;
-    }
-    getPost(); 
-}
-    
+   }
+
+   getPost(); 
+
    
